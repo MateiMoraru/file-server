a7e1aa0a5deed4b9e9bded8da70584f54c837cc3
MateiMoraru <moraru.matei24@gmail.com>
2023-11-08 07:48:46 +0000

    Add Login And SignUp Functions

--- client.py
+++ client.py
@@ -1,10 +1,81 @@
 import socket
 
 class Client:
-    def __init__(self, ip, addr):
+    ENCODING = "UTF-8"
+    BUFFER_SIZE = 4096
+
+    def __init__(self, ip: str, addr: int):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_addr = (ip, addr)
 
     
     def connect(self):
         self.socket.connect(self.server_addr)
+
+        signup = input("Do You Want To Create An Account? yes/no")
+        self.send(signup)
+
+        if signup == "yes":
+            self.signup()
+        else:
+            self.login()
+
+
+    def login(self):
+        username = input("Username: ")
+        password = input("Password: ") #Use GetPass to hide password
+        credentials = username + ' ' + password
+
+        self.send(credentials)
+
+        confirmation = self.recv()
+        
+        if confirmation == "Logged In Successfully":
+            self.user_rights = self.recv()
+            print(f"Logged In Successfully, With {self.user_rights} Rights.")
+
+        elif confirmation == "Wrong Credentials":
+            print("The Credentials You Entered Weren't Found In Our Database.\n Try Again.\n")
+            self.login()
+
+        elif confirmation == "Account Not Recognised":
+            print("\n Your Account Was Not Found In Our Database.\n Try Creating One.")
+
+    
+    def signup(self):
+        username = input("Username: ")
+        password = input("Password: ") #Use GetPass to hide password
+        credentials = username + ' ' + password
+
+        self.send(credentials)
+
+        confirmation = self.recv()
+
+        if confirmation == "Account Already Exists":
+            print(f"{confirmation}, Try Again")
+            self.signup()
+
+        else:
+            print(confirmation)
+
+        print("Please Login Now")
+
+        self.login()
+        
+
+    def send(self, message: str):
+        bytes = message.encode(self.ENCODING)
+        bytes_sent = self.socket.send(bytes)
+
+        if len(bytes) != len(bytes_sent):
+            return False
+        
+        return True
+    
+
+    def recv(self):
+        try:
+            message = self.socket.recv(self.BUFFER_SIZE).decode(self.ENCODING)
+            return message
+        except TimeoutError as e:
+            print(e)
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket, os
 
 class Server:
-    BUFFER_SIZE = 1024
+    BUFFER_SIZE = 4096
     ENCODING = "UTF-8"
 
     def __init__(self, ip: str = "127.0.0.1", port: int = 8080):        
@@ -22,9 +22,50 @@ class Server:
 
     def handle_conn(self, conn: socket.socket):
         logged = False
+        signup = self.recv(conn)
+        
+        if signup == 'yes':
+            self.signup()
+        else:
+            self.login(conn)
+
+        
+    def signup(self, conn: socket.socket):
+        credentials = self.recv(conn)
+        username = credentials.split(' ')[0]
+        password = credentials.split(' ')[1]
+
+        users_file_name = "login.txt"
+    
+        try:
+            users_file = open(users_file_name, 'r', encoding = self.ENCODING)
+
+        except FileExistsError:
+            print("User file not found, creating one")
+
+            users_file = open(users_file_name, 'w+', encoding = self.ENCODING)
+            
+        users_data = users_file.readlines()
+
+        for line in users_data:
+            if username in line:
+                data = line.split(' ')
+                
+                if username == data[0]:
+                    self.send(conn, "Account Already Exists")
+                    
+                    self.signup(conn)
+        
+        users_file = open(users_file_name, 'w', encoding = self.ENCODING)
+        users_file.write(credentials)
+        self.send(conn, "Account Created Successfully")
 
 
-    def login(self, user, password):
+    def login(self, conn: socket.socket, count: int = 0):
+        credentials = self.recv(conn)
+        username = credentials.split(' ')[0]
+        password = credentials.split(' ')[1]
+
         users_file_name = "login.txt"
     
         try:
@@ -38,13 +79,20 @@ class Server:
         users_data = users_file.readlines()
         
         for line in users_data:
-            if user in line:
+            if username in line:
                 data = line.split(' ')
                 
-                if user == data[0] and password == data[1]:
+                if username == data[0] and password == data[1]:
+                    self.send(conn, "Logged In Successfully")
                     return True
                 
-        return False
+        if count <= 2:
+            self.send(conn, "Wrong Credentials")
+
+            self.login(conn, count + 1)
+        else:
+            self.send(conn, "Account Not Recognised")
+            return False
 
 
     def send(self, conn: socket.socket, message: str):
@@ -58,6 +106,7 @@ class Server:
     
     def recv(self, conn: socket.socket):
         try:
-            conn.recv()
+            message = conn.recv(self.BUFFER_SIZE).decode(self.ENCODING)
+            return message
         except TimeoutError as e:
             print(e)
